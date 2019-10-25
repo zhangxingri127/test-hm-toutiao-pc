bff573ea4bad74f19be4b002cecde464c927f8a4
zhangxingri <zhangdou127@outlook.com>
2019-10-25 21:36:10 +0800

    login框架完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <el-card></el-card>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style>
+<style lang='less'>
+.container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url("../../assets/login_bg.jpg") no-repeat center / cover;
+  left: 0;
+  top: 0;
+  .el-card {
+    width: 400px;
+    height: 350px;
+    background-color: #fff;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
 </style>
